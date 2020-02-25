d42eabbe0c3be13a5a50a03a4eee683fe7577232
Cabral Chege <cabralowiro@gmail.com>
2020-02-25 10:41:52 +0300

    added the hide and show to display the paragraph

--- js/script.js
+++ js/script.js
@@ -1,71 +1,29 @@
-$(document).ready(function () {
-    $("#design").click(function () {
-        $("p").hide();
-        $(".designs").show();
+$(Document).ready(function(){
+    $(".design_image").click(function() {
+        $("#designs_p").show();
+        $(".design_image").hide();
+        console.log("display paragraph")
     });
-
-    $('.designs').click(function () {
-        $("p").hide();
-        $('#design').show();
+    $("#dev_p").click(function(){
+        $(".dev_img").show();
+        $("#dev_p").hide();
+    });   
+    $(".dev_img").click(function() {
+        $("#dev_p").show();
+        $(".dev_img").hide();
+        console.log("display paragraph")
     });
-
-    $("#devLogo").click(function () {
-        $(this).hide();
-        $(".dev").show()
+    $("#dev_p").click(function(){
+        $(".dev_img").show();
+        $("#dev_p").hide();
+    });  
+    $(".prod_img").click(function() {
+        $("#prod_p").show();
+        $(".prod_img").hide();
+        console.log("display paragraph")
     });
-
-    $('.dev').click(function () {
-        $(this).hide();
-        $('#devLogo').show()
-    });
-
-    $("#productLogo").click(function () {
-        $(this).hide();
-        $('.pro').show();
-    });
-
-    $('.pro').click(function () {
-        $(this).hide();
-        $('#productLogo').show();
-    });
-
-    $('.first').hover(function () {
-        $('#1').toggle();
-    });
-
-
-    $('.second').hover(function () {
-        $('#2').toggle();
-    });
-
-    $('.third').hover(function () {
-        $('#3').toggle();
-    });
-
-    $('.fourth').hover(function () {
-        $('#4').toggle();
-    });
-
-
-    $('.fifth').hover(function () {
-        $('#5').toggle();
-    });
-
-    $('.sixth').hover(function () {
-        $('#6').toggle();
-    });
-
-    $('.seventh').hover(function () {
-        $('#7').toggle();
-    });
-
-    $('.eighth').hover(function () {
-        $('#8').toggle();
-    });
-
-    $(".button").click(function(){
-        var name = document.getElementById('mce-EMAIL').value ;
-        alert('Hello '+ name +", Thak you for reaching out to us.We will get back to you as soon as possible.")
-    });
-
+    $("#prod_p").click(function(){
+        $(".prod_img").show();
+        $("#prod_p").hide();
+    });    
 });
