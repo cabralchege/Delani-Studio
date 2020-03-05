3c8b06f83880773d8b893fa0ea7ca6ee4cea07f4
Cabral Chege <cabralowiro@gmail.com>
2020-03-05 06:29:30 +0300

    added the hover effect in the js to make images of the portfolio hover

--- js/script.js
+++ js/script.js
@@ -26,12 +26,87 @@ $(Document).ready(function(){
         $(".prod_img").show();
         $("#prod_p").hide();
     });
-    function submitForm(){
-        var name = document.getElementById("name").value;
-        var email =document.getElementById("email").value;
-        var message =document.getElementById("message".value)
-    }
-    
 
     
-});
+    $(".four").hover(function () {
+        $("#overlay4").show();
+      })
+      $(".four").mouseleave(function () {
+        $("#overlay4").hide();
+      })
+    
+      $(".three").hover(function () {
+        $("#overlay3").show();
+      })
+      $(".three").mouseleave(function () {
+        $("#overlay3").hide();
+      })
+    
+    
+      $(".two").hover(function () {
+        $("#overlay2").show();
+      })
+      $(".two").mouseleave(function () {
+        $("#overlay2").hide();
+      })
+    
+      $(".one").hover(function () {
+        $("#overlay1").show();
+      })
+      $(".one").mouseleave(function () {
+        $("#overlay1").hide();
+      })
+    
+      $(".five").hover(function () {
+        $("#overlay5").show();
+      })
+      $(".five").mouseleave(function () {
+        $("#overlay5").hide();
+      })
+    
+      $(".six").hover(function () {
+        $("#overlay6").show();
+      })
+      $(".six").mouseleave(function () {
+        $("#overlay6").hide();
+      })
+    
+      $(".seven").hover(function () {
+        $("#overlay7").show();
+      })
+      $(".seven").mouseleave(function () {
+        $("#overlay7").hide();
+      })
+    
+      $(".eight").hover(function () {
+        $("#overlay8").show();
+      })
+      $(".eight").mouseleave(function () {
+        $("#overlay8").hide();
+      })
+      
+      event.preventDefault();
+    
+    });  
+    $(document).ready(function () {
+        $('.overlay').mouseover(function () {
+            $('#text').show();
+        }).mouseout(function () {
+            $('#text').hide();
+        });
+    });
+        
+    $(document).ready(function () {
+        $('#submit').click(function () {
+            var Name = $('#Name').val();
+            var Email = $('#Email').val();
+            var Message = $('#Message');
+            var key ='43ab10333d46e19325f96d210ed40c49-us5';
+            if (Name == '' || Email == '' || Message == '') {
+                alert('Please make sure you have filled in the form correctly!');
+            } else {
+                alert(' Hi ' + Name + ' We have received your message. Thank you for reaching out to us.');
+            }
+    
+        });
+    });
